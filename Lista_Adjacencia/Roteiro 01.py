1b17049e94f459c84e66248ac240904681cf3288
hevlla <32911113+hevlla@users.noreply.github.com>
2018-05-03 23:46:22 -0300

    Grafo possui agora enconta o maior caminho.

--- Lista_Adjacencia/Roteiro 01.py
+++ Lista_Adjacencia/Roteiro 01.py
@@ -5,9 +5,10 @@ from grafo import Grafo
 def Inicio():
 
     dicio = {}
-    vertices = input("").split(", ") #Informe os vertices:
 
-    arestas = input("").split(", ") #Informe as arestas:
+    vertices = input("Infome os vertices: ").split(", ") #Informe os vertices:
+
+    arestas = input("Informe as arestas: ").split(", ") #Informe as arestas:
 
     for i in range(len(arestas)):
         separando = arestas[i].split("(")
@@ -25,7 +26,7 @@ def Menu():
           " d - Informe o grau de vértice: \n"
           " e - Quais arestas incidem sobre o vértice: \n"
           " f - O grafo é completo: \n"
-          " g - Desafio: \n"
+          " g - Maior caminho: \n"
           " h - Sair: \n")
 
 def VerticesUsados(arestas):
@@ -43,19 +44,21 @@ def VerticesUsados(arestas):
     return adjacente
 
 #Opcao a
-def ParesAjacentes(vertices, arestas, adjacente) :
+def ParesAjacentes(vertices, arestas):
     vertices = inicio[0]
     arestas = inicio[1].values()
-    adjacente = VerticesUsados(arestas)
-    v_n_adj = ""
+    completo = []
+    resultado = " "
 
-    for i in vertices:
-        if i not in adjacente:
-            v_n_adj += "\""
-            v_n_adj += i
-            v_n_adj += "\" "
+    for x in vertices:
+        for z in vertices:
+            if (x + "-" + z) not in arestas and (z + "-" + x) not in arestas :
+                if x != z:
+                    if (z + "-" + x) not in completo :
+                        completo.append(x + "-" + z)
+                        resultado += ("\n" + x + "-" + z)
 
-    return v_n_adj
+    return resultado
 
 #Opcao b
 def VerticeAdjacente(arestas):
@@ -86,15 +89,16 @@ def ArestasParalelas(arestas, v_adjacente):
             for j in v_adjacente:
                 contador += palavra.count((i + "-" + j))
                 contador += palavra.count((j + "-" + i))
-            if contador >= 2:
-                return True
-            else:
-                contador = 0
-        return False
+                if contador >= 2:
+                    return True
+                else:
+                    contador = 0
+    return False
 
 #Opcao d
 def GrauDoVertice(busca, arestas):
     vertice = busca
+    print(vertice)
     arestas = inicio[1].values()
     adjacente = 0
 
@@ -102,9 +106,11 @@ def GrauDoVertice(busca, arestas):
         palavra = j
         if vertice == palavra[0] and palavra[0] != palavra[2]:
             adjacente += 1
+            print(adjacente)
 
         if vertice == palavra[2] and palavra[2] != palavra[0]:
             adjacente += 1
+            print(adjacente)
 
     return str(adjacente)
 
@@ -155,54 +161,99 @@ def GrafoCompleto(arestas, v_adjacente ):
                 break
     return completo
 
+def DicionarioAdjacencia(vertices, arestas):
+    arestas = inicio[1].values()
+    dici = {}
+
+    for i in vertices:
+        dici[i] = set()
+
+        for x in arestas:
+            palavra = x
+            if palavra[0] == i:
+                dici[i].add(palavra[2])
+            if palavra[2] == i:
+                dici[i].add(palavra[0])
+    return dici
+
+def Comprimento(grafo, init, fim):                                                                                                                              # Algoritmo de busca Depth Dirst Search
+    caminho = [(init, [init])]
+
+    while caminho:
+        (vertice, percorrer ) = caminho.pop()
+
+        for o in grafo[vertice] - set(percorrer):
+            if o == fim:
+                yield percorrer + [o]
+            else:
+                caminho.append((o, percorrer + [o]))
+
 #Opcao g
-def Desafio():
-    print()
+def DesafioComprimento(vertices, dicionario):                                                                                                                   #Bagaca so da raiva
+    encontra_maior_caminho = 0
+    for i in vertices:
+        for j in vertices:
+            if i != j:
+                for c in list(Comprimento(dicionario, i, j)):
+                    if len(c) - 1 > encontra_maior_caminho:
+                        encontra_maior_caminho = len(c) - 1
+
+    return encontra_maior_caminho
 
 
 inicio = Inicio()
 itens = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
 while True:
 
-    menu = Menu()
-    print("Informe uma letra: ")
-    escolha = input().lower()
-    grafo = Grafo(inicio[0], inicio[1])
+    try:
 
-    if escolha not in itens:
-        print("\n\""+ escolha + "\" : Esta letra não esta no menu.\n")
-    else:
-        if escolha == "a" :
-            verticePresentes = VerticesUsados(inicio[1])
-            adjacente = ParesAjacentes(inicio[0], inicio[1], verticePresentes)
-            print("Pares de Vértices não adjacentes:",adjacente + "\n")
+        grafo = Grafo(inicio[0], inicio[1])
+        menu = Menu()
+        print("Informe uma letra: ")
+        escolha = input().lower()
 
-        elif escolha == "b":
-            laco = VerticeAdjacente(inicio[1])
-            print("Vértice adjacente a ele mesmo:",laco, "\n")
+        if escolha not in itens:
+            print("\n\""+ escolha + "\" : Esta letra não esta no menu.\n")
 
-        elif escolha == "c":
-            verticePresentes = VerticesUsados(inicio[1])
-            aParalelas = ArestasParalelas(inicio[1], verticePresentes)
-            print("Há arestas paralelas: " + str(aParalelas) + "\n")
+        else:                                                                                                           #Informe os pares que não são adjacentes
+            if escolha == "a" :
+                adjacente = ParesAjacentes(inicio[0], inicio[1])
+                print("Pares de Vértices não adjacentes:",adjacente + "\n")
 
-        elif escolha == "d":
-            buscaG = input("Informe um vertice: ")
-            grauVertice = GrauDoVertice(buscaG, inicio[1])
-            print("Grau de vértice: " + grauVertice + "\n")
+            elif escolha == "b":
+                laco = VerticeAdjacente(inicio[1])
+                print("Vértice adjacente a ele mesmo:",laco, "\n")
 
-        elif escolha == "e":
-            busca = input("Informe um vertice: ")
-            aIncidentes = ArestasIncidentes(busca, inicio[1])
-            print("Estas arestas incidem sobre o vértice: " + aIncidentes + "\n")
+            elif escolha == "c":
+                verticePresentes = VerticesUsados(inicio[1])
+                aParalelas = ArestasParalelas(inicio[1], verticePresentes)
+                print("Há arestas paralelas: " + str(aParalelas) + "\n")
 
-        elif escolha == "f":
-            verticePresentes = VerticesUsados(inicio[1])
-            completo = GrafoCompleto(inicio[1],verticePresentes)
-            print("O grafo é completo: " + str(completo) + "\n")
+            elif escolha == "d":
+                buscaG = input("Informe um vertice: ").upper()
+                grauVertice = GrauDoVertice(buscaG, inicio[1])
+                print("Grau de vértice: " + grauVertice + "\n")
 
-        elif escolha == "g":
-            desafio = Desafio()
+            elif escolha == "e":
+                busca = input("Informe um vertice: ").upper()
+                aIncidentes = ArestasIncidentes(busca, inicio[1])
+                print("Estas arestas incidem sobre o vértice: " + aIncidentes + "\n")
 
-        elif escolha == "h":
-            break
+            elif escolha == "f":
+                verticePresentes = VerticesUsados(inicio[1])
+                completo = GrafoCompleto(verticePresentes, inicio[1])
+                print("O grafo é completo: " + str(completo) + "\n")
+
+            elif escolha == "g":
+                verticePresente = VerticesUsados(inicio[1])
+                listaadjacente =  DicionarioAdjacencia(verticePresente, inicio[1])
+                desafio = DesafioComprimento(verticePresente, listaadjacente)
+                print("O maior caminho é: " + str(desafio) + "\n")
+
+            elif escolha == "h":
+                break
+
+    except:
+        print("Informe valores válidos")
+        inicio = Inicio()
